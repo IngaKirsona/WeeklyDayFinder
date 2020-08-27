1757fcae85c846d7d6c89ee22df80ebc695b8c8f
IngaKirsona <ingas@inbox.lv>
2020-08-27 18:16:43 +0300

    Teachers version of this homework.

--- WeeklyDayFinder/ViewController.swift
+++ WeeklyDayFinder/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var resultLabel: UILabel!
 
+    @IBOutlet weak var findButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -29,13 +30,11 @@ func handleCalculation(){
         let calendar = Calendar.current
         var dateComponent = DateComponents()
     
-    
-    guard let day = dayTextField.text, !day.isEmpty, Int(day) ?? 0 <= 31, let month = monthTextField.text, !month.isEmpty, Int(month) ?? 0 <= 12, let year = yearTextField.text, !year.isEmpty
-            else {
-            print ("textField error")
-            self.resultLabel.text = "Input error!"
-            return
-        }
+    guard let day = Int(dayTextField.text!), let month = Int(monthTextField.text!), let year = Int(yearTextField.text!)
+        else {
+        warningPopup(withTitle: "Input Error!", withMessage: "Date Text Fields can't be Empthy!")
+        return
+            }
     
         dateComponent.day = Int(day)
         dateComponent.month = Int(month)
@@ -43,18 +42,50 @@ func handleCalculation(){
     
         
     guard let date = calendar.date(from: dateComponent)
-            else {
+        else {
             print ("calendar.date error")
-            self.resultLabel.text = "Input error!"
+        self.resultLabel.text = "Input error!"
             return
         }
     
         let dateFormatter = DateFormatter()
         dateFormatter.locale = Locale(identifier: "en_EN")
         dateFormatter.dateFormat = "EEEE"
-            
+    
+    switch findButton.titleLabel?.text {
+    case "Find":
+        findButton.setTitle("Clear", for: .normal)
+        if day >= 1 && day <= 31 && month >= 1 && month <= 12 {
+        
         let weekday = dateFormatter.string(from: date)
-        resultLabel.text = weekday
+            let captalizedWeekday = weekday.capitalized
+        resultLabel.text = captalizedWeekday
+            
+        }else {
+        warningPopup(withTitle: "Wrong Date!", withMessage: "Please enter correct date.")
+        }
+    default:
+        findButton.setTitle("Find", for: .normal)
+        clearTextField()
+        }
+    }
+    
+    func clearTextField(){
+        dayTextField.text = ""
+        monthTextField.text = ""
+        yearTextField.text = ""
+        resultLabel.text = "Find a weekday"
+        
+    }
+    
+    func warningPopup(withTitle title: String, withMessage message: String?){
+        
+        DispatchQueue.main.async {
+            let popUp = UIAlertController(title: title, message: message, preferredStyle: .alert)
+            let okButton = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            popUp.addAction(okButton)
+            self.present(popUp, animated: true, completion: nil)
+        }
     }
     
 @IBAction func findWeekDayTapped(_ sender: Any) {
