5edefc95471a078e7139e1185669148899a81df2
IngaKirsona <ingas@inbox.lv>
2020-08-27 20:04:00 +0300

    added two more screens

---
+++ WeeklyDayFinder/AppearenceViewController.swift
@@ -0,0 +1,21 @@
+//
+//  AppearenceViewController.swift
+//  WeeklyDayFinder
+//
+//  Created by Inga Kirsona on 27/08/2020.
+//  Copyright © 2020 Inga Kirsona. All rights reserved.
+//
+
+import UIKit
+
+class AppearenceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+ 
+    }
+    
+
+
+}
