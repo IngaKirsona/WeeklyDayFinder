f3e4d924f0b314ceadc423842e396114b5ae825c
IngaKirsona <ingas@inbox.lv>
2020-08-27 21:09:18 +0300

    light/dark mode

--- WeeklyDayFinder/AppearenceViewController.swift
+++ WeeklyDayFinder/AppearenceViewController.swift
@@ -10,12 +10,35 @@ import UIKit
 
 class AppearenceViewController: UIViewController {
 
+    @IBOutlet weak var textLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
- 
     }
-    
+    @IBAction func openSettingsTapped(_ sender: Any) {
+        guard let SettingsURL = URL(string: UIApplication.openSettingsURLString) else{
+            return
+        }
+        if UIApplication.shared.canOpenURL(SettingsURL){
+            UIApplication.shared.open(SettingsURL, options: [:]) {(success) in
+            print(success)
+            }
+        }
+    }
+    func setLabelText(){
+        var text = "Unable to specify UI style"
+        if self.traitCollection.userInterfaceStyle == .dark {
+            text = "Dark Mode is On. \nGo to settings if you would like \n to change to Light Mode."
+        }else{
+            text = "Light Mode is On. \nGo to settings if you would like \n to change to Dark Mode."
+        }
+        textLabel.text = text
+    }
+}
 
-
+extension AppearenceViewController {
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        setLabelText()
+    }
 }
